fc49637637211e34d2fbd9ed4c5fba92874b18cc
gavrielhan <102911767+gavrielhan@users.noreply.github.com>
2025-02-26 12:51:04 +0200

    config file

---
+++ config_local.py
@@ -0,0 +1,7 @@
+# config_local.py
+
+# Set base to your desired base directory
+base = '/Users/gavrielhannuna/PycharmProjects/MSc_Thesis/base'
+
+# Override qc_dir to a writable location (for example, inside your MSc_Thesis folder)
+qc_dir = '/Users/gavrielhannuna/PycharmProjects/MSc_Thesis/qc'
